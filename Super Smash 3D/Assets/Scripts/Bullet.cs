77388c96a3ca2e16af66375f1522d8e0ceaafcad
z1ncx <aidench703@gmail.com>
2025-12-26 18:03:35 -0500

    ver 0.5.1
    some buggy weird knockback stuff, bullet now travels instead of being... dysfunctional?

--- Super Smash 3D/Assets/Scripts/Bullet.cs
+++ Super Smash 3D/Assets/Scripts/Bullet.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject nerfgun;
-
+    private Rigidbody rb;
+    public float bulletSpeed;
+    public float enemyKnockback;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * bulletSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "enemy")
         {
-            print("yo dis works twin you up");
+            Rigidbody rbE = other.gameObject.GetComponent<Rigidbody>();
+            // print(rbE.mass);
+            Vector3 kbVector = transform.forward * enemyKnockback;
+            rbE.AddForce(kbVector);
+            print(kbVector);
         }
     }
 }
