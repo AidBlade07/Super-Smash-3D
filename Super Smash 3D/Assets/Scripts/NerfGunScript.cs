b999f71b6a5af2e9500c0f98e89a93e2709fb373
AidBlade07 <aidench703@gmail.com>
2026-02-20 02:22:48 -0500

    ver 0.5.9
    mostly stuff behind the scenes, new pistol sense the old one was tweaking out often...
    
    starting weapon switching, mesh renderer on the pistol seems to be messed up..
    
    this isnt gonna be the only push today, just felt like doing this on my own time.

--- Super Smash 3D/Assets/Scripts/NerfGunScript.cs
+++ Super Smash 3D/Assets/Scripts/NerfGunScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,33 +18,110 @@ public class NerfGunScript : MonoBehaviour
     public Transform camT;
     private bool reloading;
     private bool spinning;
+    public bool isPistol;
+    public bool isAssaultRifle;
+    public float assaultRifleRate;
+    public bool attemptRaycast;
+    public int activeWeapon;
+    public int totalWeapons;
+    public int weaponId;
     // Start is called before the first frame update
     void Start()
     {
+        // if clip is extended
         if(extendedClipLight)
         {
+            // change max clip size to 25
             ammoLight = 25;
         }
+        
+        // the gun is not reloading at the start of the game.
         reloading = false;
-        ammoL.text = ammoLight + "/" + reserveLightAmmo;
+        ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // is player actively on wpn slot 0?
+        if (activeWeapon == 0 && isPistol)
+        {
+            SendMessage("SpawnPistol");
+        }
+        else
+        {
+            SendMessage("DespawnPistol");
+        }
+        
+
+        // is player actively on wpn slot 1?
+        if (activeWeapon == 1 && isAssaultRifle)    
+        {
+            SendMessage("SpawnGun");
+        }
+        else
+        {
+            SendMessage("DespawnGun");
+        }
+        
+
+        // if the wpn slot goes ABOVE the amount of weapons the player has;
+        if (activeWeapon > totalWeapons)
+        {
+            // set the active wpn slot back to zero, creating a loop effect
+            activeWeapon = 0;
+        }
+        // if the wpn slot goes ABOVE the amount of weapons the player has;
+        if (activeWeapon < 0)
+        {
+            // set the active wpn slot to the total weapons you have, continuing the loop effect
+            activeWeapon = totalWeapons;
+        }
+        
+        if (Input.mouseScrollDelta.y > 0)
+        {            
+            activeWeapon += 1;
+        }
+        
+        if (Input.mouseScrollDelta.y < 0)
+        {            
+            activeWeapon -= 1;
+        }
+        /*
+        if (isAssaultRifle)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.enabled = true;
+        }
+        else
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.enabled = false;
+        }
+        if (isPistol)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.enabled = true;
+        }
+        else
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.enabled = false;
+        }
+        */
         if (extendedClipLight)
         {
             lightMagSize = 25;
-            ammoL.text = ammoLight + "/" + reserveLightAmmo;
+            ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
         }
         else
         {
             lightMagSize = 12;
-            ammoL.text = ammoLight + "/" + reserveLightAmmo;
+            ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
         }
 
-        //check if left click shot
-        if (Input.GetMouseButtonDown(0))
+//      check if left click shot           is this weapon a pistol?
+        if (Input.GetMouseButtonDown(0) && isPistol)
         {
             //have you clicked the reload keybind in the last 2 seconds
             if (!reloading)
@@ -53,24 +131,34 @@ public class NerfGunScript : MonoBehaviour
                 {
                     RaycastHit hit;
                     GameObject newBullet = Instantiate(bullet, transform.position + transform.up * 0.6f + transform.forward * 0.8f, transform.rotation);
-                    if (Physics.Raycast(camT.position, camT.forward, out hit, 200000f))
+                    if (attemptRaycast)
                     {
-                        print(hit.distance);
-                        newBullet.transform.LookAt(hit.point);
+                        if (Physics.Raycast(camT.position, camT.forward, out hit, 200000f))
+                        {
+                            print(hit.distance);
+                            newBullet.transform.LookAt(hit.point);
+                        }
+                        else
+                        {
+                            print("code aint locked in");
+                        }
                     }
-                    else
-                    {
-                        print("code aint locked in");
-                    }
-
-
                     ammoLight -= 1;
-                    ammoL.text = ammoLight + "/" + reserveLightAmmo;
+                    ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
                 }
             }
         }
 
-        if(spinning)
+//      check if left click held       is this weapon an AR?
+        if (Input.GetMouseButton(0) && isAssaultRifle)
+        {
+            InvokeRepeating(nameof(WeaponFire), 0, assaultRifleRate);
+        }
+        else
+        {
+            CancelInvoke(nameof(WeaponFire));
+        }
+        if (spinning)
         {
             spinOffset.transform.Rotate(spinSpeed * Time.deltaTime, 0, 0);
         }
@@ -86,12 +174,45 @@ public class NerfGunScript : MonoBehaviour
             Invoke(nameof(ReloadLight), 2f);
         }
     }
+
+    private void Invoke(string v)
+    {
+        throw new NotImplementedException();
+    }
+
     void ReloadLight()
     {
         reserveLightAmmo -= (lightMagSize - ammoLight);
         ammoLight = lightMagSize;
-        ammoL.text = ammoLight + "/" + reserveLightAmmo;
+        ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
         reloading = false;
         spinning = false;
+    }
+
+    void WeaponFire()
+    {
+        //have you clicked the reload keybind in the last 2 seconds
+        if (!reloading)
+        {
+            //is the mag size above 0
+            if (ammoLight > 0)
+            {
+                RaycastHit hit;
+                GameObject newBullet = Instantiate(bullet, transform.position + transform.up * 0.6f + transform.forward * 0.8f, transform.rotation);
+                if (Physics.Raycast(camT.position, camT.forward, out hit, 200000f))
+                {
+                    print(hit.distance);
+                    newBullet.transform.LookAt(hit.point);
+                }
+                else
+                {
+                    print("code aint locked in");
+                }
+
+
+                ammoLight -= 1;
+                ammoL.text = ammoLight + "/" + reserveLightAmmo + "         " + activeWeapon;
+            }
+        }
     }
 }
--- Super Smash 3D/Assets/Scripts/QuoteHandsCode.cs
+++ Super Smash 3D/Assets/Scripts/QuoteHandsCode.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class QuoteHandsCode : MonoBehaviour
 {
+    public int activeWeapon;
+    public int totalWeapons;
     public Transform playerT;
     public Transform camT;
     // Start is called before the first frame update
@@ -12,7 +15,31 @@ public class QuoteHandsCode : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // LateUpdate is run immediately after an Update Event
+
+    // run every frame
+    void Update()
+    {
+    /*
+        if (activeWeapon > totalWeapons)
+        {
+            activeWeapon = 0;
+        }
+        if (activeWeapon < 0)
+        {
+            activeWeapon = totalWeapons;
+        }
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            activeWeapon += 1;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            activeWeapon -= 1;
+        }
+    */
+    }
     void LateUpdate()
     {
         transform.position = playerT.position;
