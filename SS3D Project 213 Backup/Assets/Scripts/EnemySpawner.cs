0a4911c8565174b7eedad254ab605ebcd88b321e
z1ncx <aidench703@gmail.com>
2026-06-26 18:06:06 -0400

    ver 0.7.6
    most definitely broken/missing an asset...
    
    starting getting the wave text in the bottom working, that might be done next time.

--- SS3D Project 213 Backup/Assets/Scripts/EnemySpawner.cs
+++ SS3D Project 213 Backup/Assets/Scripts/EnemySpawner.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using JetBrains.Annotations;
+using System;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Serializable]
+    public struct WaveData
+    {
+        public GameObject prefab;
+        public int count;
+    }
+
+    [Serializable]
+    public struct RoundData
+    {
+        public WaveData[] waveData;
+    }
+    public RoundData[] rounds;
+    
     public int enemiesInWave;
     public int wave;
     public int enemiesRemaining;
@@ -20,9 +36,20 @@ public class EnemySpawner : MonoBehaviour
     {
         InvokeRepeating(nameof(CheckIfWave), 2.0f, 1.0f);
     }
+
+    void StartRound()
+    {
+        RoundData currentRound = rounds[wave];
+        for(int i = 0; i < currentRound.waveData[0].count; i++)
+        {
+            Instantiate(currentRound.waveData[0].prefab);
+        }
+          
+    }
+
     void SpawnEnemy()
     {
-        int randPlat = Random.Range(0, 3);
+        int randPlat = UnityEngine.Random.Range(0, 3);
         GameObject choicePlat = spawnPlats[randPlat];
         Vector3 spawnPosition = choicePlat.transform.position + new Vector3(0, 0.01f, 0);
         GameObject newE = Instantiate(enemyBean, spawnPosition, Quaternion.identity);
